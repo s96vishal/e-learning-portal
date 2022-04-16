fa6a6e7186f806d89045e6c613a2d85e30c620c9
VISHAL ARUN SHRIVASTAVA <vshr9910@india.convergys.com>
2022-04-16 12:27:46 +0530

    Code for FE

---
+++ src/components/Footer.js
@@ -0,0 +1,102 @@
+import React, { Component } from "react";
+
+class Footer extends Component {
+  render() {
+    return (
+      <div>
+        {/*====================  footer area ====================*/}
+        <div className="footer-area dark-bg">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-12">
+                <div className="footer-content-wrapper section-space--inner--100">
+                  <div className="row">
+                    <div className="col-xl-3 col-lg-3 col-md-12">
+                      {/* footer intro wrapper */}
+                      <div className="footer-intro-wrapper">
+                        <div className="footer-logo">
+                          <a href={`${process.env.PUBLIC_URL}/home-two`}>
+                            <div className="logoHead">
+                              <img
+                                src="/assets/img/logo/logonew.png"
+                                alt=""
+                                className="sticky-logo img-fluid"
+                              />
+                              <h3> E-Learning Portal</h3>
+                            </div>
+                          </a>
+                        </div>
+                        <div className="footer-desc">
+                         E Learning Portal - A Project Build
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-xl-2 offset-xl-1 col-lg-3 col-md-4">
+                      {/* footer widget */}
+                      <div className="footer-widget">
+                        <h4 className="footer-widget__title">USEFUL LINKS</h4>
+                        <ul className="footer-widget__navigation">
+                          <li>
+                            <a href={`${process.env.PUBLIC_URL}/`}>Home</a>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                    <div className="col-xl-2 offset-xl-1 col-lg-3 col-md-4">
+                      {/* footer widget */}
+                      <div className="footer-widget">
+                        <h4 className="footer-widget__title">USEFUL LINKS</h4>
+                        <ul className="footer-widget__navigation">
+                          <li>
+                            <a href={`${process.env.PUBLIC_URL}/`}>
+                              Go To Home
+                            </a>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                    <div className="col-xl-2 offset-xl-1 col-lg-3 col-md-4">
+                      {/* footer widget */}
+                      <div className="footer-widget mb-0">
+                        <h4 className="footer-widget__title">CONTACT US</h4>
+                        <div className="footer-widget__content">
+                          <p className="address">
+                            E- Learning Portal Address
+                          </p>
+                          <ul className="contact-details">
+                            <li>
+                              <span>P:</span>999999999
+                            </li>
+                            <li>
+                              <span>F:</span>5555555
+                            </li>
+                            <li>
+                              <span>E:</span>elearningportal@gmail.com
+                            </li>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="footer-copyright-wrapper">
+            <div className="footer-copyright text-center">
+              Copyright © 2022. All right reserved
+            </div>
+          </div>
+        </div>
+        {/*====================  End of footer area  ====================*/}
+        {/*====================  scroll top ====================*/}
+        <button className="scroll-top" id="scroll-top">
+          <i className="ion-android-arrow-up" />
+        </button>
+        {/*====================  End of scroll top  ====================*/}
+      </div>
+    );
+  }
+}
+
+export default Footer;
